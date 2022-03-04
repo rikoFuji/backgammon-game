bbb3507085d0a8c16e08401b9ffd8665060aac51
rikoFuji <98547791+rikoFuji@users.noreply.github.com>
2022-03-03 19:11:43 -0800

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+
 public class Player {
+    private String color;
+    private int move;
+    private int 
+
+
+    public Player(String c){
+        color = c;
+    }
+
+    public String firstRoll(){
+        System.out.println("Who play first?");
+        Scanner kbd = new Scanner(System.in);
+        String first = kbd.nextLine();
+        //ask black, white, computer
+        return first;
+    }
+
+    public int[] RollTurn(){
+        int dice[]=new int[2];
+        dice[0] = Dice.roll();
+        dice[1] = Dice.roll();
+
+        //SelectMove(dice1,dice2);
+        return dice;
+    }
+
+    public int SelectMove(int d1, int d2){
+        Move.getpossibleMoves(color, d1, d2);
+        System.out.println("Select move from the list");
+        Scanner kbd = new Scanner(System.in);
+        int move = kbd.nextInt();
+        //ask the move they want from the possible move
+        return move;
+    }
+
 }
+
